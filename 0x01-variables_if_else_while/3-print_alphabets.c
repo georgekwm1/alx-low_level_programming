724f32c265ad221b5fbe34683d5ce9f00c3e8f84
root <georgekwm1@gmail.com>
2022-09-09 22:06:32 -0700

    C codes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+char ch
+
+
+        for (ch = 'a'; ch <= 'z';)
+	  {
+        putchar("%c \n", ch);
+        ch++;
+	  }
+
+        for (ch = 'A'; ch <= 'Z';)
+	  {
+        putchar("%c \n", ch);
+        ch++;
+	  }
+
+}
