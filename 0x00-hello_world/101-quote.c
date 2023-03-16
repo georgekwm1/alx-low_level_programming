b8f452252eb2fca4e6a0b8f204a28d13cd7e456e
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-16 14:53:33 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@ char d[] = "Dora Korpar";
 int i = 2015;
 int j = 10;
 int k = 19;
-
-printf( "%s\" - %s, %d-%d-%d\n", c, d, i, j, k );
+printf("%s\" - %s, %d-%d-%d\n", c, d, i, j, k);
 return(1);
 }
