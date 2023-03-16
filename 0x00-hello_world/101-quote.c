4736a4b2ecd2e2096fbcad285d20734a95fb2bcc
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-16 14:44:31 -0700

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+char c[]= "and that piece of art is useful";
+char d[]= "Dora Korpar";
+int i=2015;
+int j=10;
+int k=19;
+
+printf("%s\" - %s, %d-%d-%d\n",c,d,i,j,k);
+return(1);
+}
