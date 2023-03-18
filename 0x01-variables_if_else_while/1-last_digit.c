866f5471ed2db6baf587d4646b850fe3a4bc5cb3
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-18 07:53:46 -0700

    my commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,5 +29,5 @@ printf("Last digit of %d is %d and is 0\n", n, ld);
 
 else if (ld < 6 && ld != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
-return (n);
+return (0);
 }
