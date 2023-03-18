0538b6e8a364d9ae686eefee28cbc246e7c33b93
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-18 01:52:20 -0700

    my commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
-int main() {
-    int n;
-    n = 0;
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * : describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+n = 0;
 
-    while (n<10){
-        putchar(n + '0');
-        n++;
-    }
-    putchar('\n');
-    return 0;
+while (n<10)
+{
+putchar(n + '0');
+n++;
+}
+putchar('\n');
+return (0);
 }
