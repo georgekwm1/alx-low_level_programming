39e1a4f53e20260374949b2c17673aa4a2858053
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-18 02:02:46 -0700

    my commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * : describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 16; i++)
+{
+printf("%x\n", i);
+}
+return (0);
+}
