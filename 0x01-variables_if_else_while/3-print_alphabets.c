8f9a3606935d34ef2584aaeba326c8f514cb47aa
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-18 02:46:43 -0700

    my commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,16 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * : describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char lower,upper;
 
-for (lower = 'a'; lower <= 'z'; lower++) {
-        putchar(lower);
-    }
-    for (upper = 'A'; upper <= 'Z'; upper++) {
-        putchar(upper);
-    }
-    putchar('\n');
-    return 0;
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+putchar(lower);
+}
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+putchar(upper);
+}
+putchar('\n');
+ return (0);
 }
