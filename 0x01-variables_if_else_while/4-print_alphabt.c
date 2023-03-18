e68c415a63e17c79b937af4de3fb0cf25ad84fe5
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-18 01:15:17 -0700

    my commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+/* more headers goes there */
 #include <stdlib.h>
 #include <time.h>
+/* betty style doc for function main goes there */
 
-int main() {
-    char letter;
-
-    for (letter = 'a' ; letter <= 'z' ; letter++) {
-        if (letter == 'c'|| letter == 'q'){
-            putchar(32);
-        }
-	putchar(letter);  
-    }
-    putchar('\n');
-    return 0;
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar('\n');
+return (0);
 }
