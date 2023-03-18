2dc64a7a85b46ccaf04f231d7bca76f066846892
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-18 01:41:40 -0700

    my commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 /* betty style doc for function main goes there */
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * : describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)/*this is the start of the code*/
 {
 int n;
