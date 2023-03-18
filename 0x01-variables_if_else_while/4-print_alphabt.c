dfb25ff73f7b7ad37544ffdecfe70e1526a08bd3
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-18 01:18:12 -0700

    my commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 /* betty style doc for function main goes there */
-
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char letter = 'a';
