493f064af19325d126223b8ce5348d3c0dfa8474
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-19 05:27:00 -0700

    my commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * : describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+int i,j;
+
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 0 ; j < 10 ; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+
+}
+return 0;
+}
