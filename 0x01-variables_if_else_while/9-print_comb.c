19a55bf6174af76c71ee7ceabe81f520c0dc28bd
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-19 05:32:40 -0700

    my commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,17 +14,14 @@
  */
 int main(void)
 {
-int i, j;
+int i;
 
 for (i = 0 ; i < 10 ; i++)
 {
-for (j = 0 ; j < 10 ; j++)
-{
+
 putchar(i + '0');
-putchar(j + '0');
 putchar(',');
 putchar(' ');
-}
 
 }
 return (0);
