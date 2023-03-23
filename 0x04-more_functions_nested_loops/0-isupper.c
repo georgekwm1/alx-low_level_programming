e6c79406ccded7c833dee133327e2b0fa384b1aa
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-23 15:16:15 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+if (c= 'A')
+{
+return (1);
+}
+return (0);
+}
