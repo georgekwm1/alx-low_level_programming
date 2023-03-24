a1888b6168510d7526aac584a499fc0464dcc05c
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-24 00:52:30 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+int multiply = a * b;
+_putchar(multiply + '0');
+return (0);
+}
