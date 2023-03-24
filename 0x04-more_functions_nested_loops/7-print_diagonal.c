d7599a10526ad59e339bdfe0f7d2fc0fdaf56c38
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-24 13:52:59 -0700

    my commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,10 +10,19 @@ if (n <= 0)
 _putchar('\n');
 else
 {
-int i;
+int i, j;
 
-for (i > 1; i <= n; i++)
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (j == i)
 _putchar('\\');
+else if (j < i)
+_putchar(' ');
 }
-_putcar('\n');
+_putchar('\n');
 }
+}
+}
+
