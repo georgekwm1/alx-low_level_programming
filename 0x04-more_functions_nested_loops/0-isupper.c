a425c917b3e6b5b4ebb158677f29d381e004c829
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-24 01:21:18 -0700

    my commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,10 @@
 
 /* betty style doc for function main goes there */
 /**
-* main - print putchar
-* Return: always 0
-*/
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
