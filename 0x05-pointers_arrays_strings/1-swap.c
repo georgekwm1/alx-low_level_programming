e8be6b860edeedbeceec7bfc90ca3e3da0919e9f
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-27 13:42:25 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Writes a function that swaps the values of two integers
+ *@a: Variable
+ *@b: Variable
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+*a = &(*b);
+*b = &(*a);
+
+}
