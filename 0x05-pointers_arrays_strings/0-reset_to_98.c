b61ecfc8e62f98991a93d41745d74881a64413fa
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-27 12:45:39 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98 - Write a function that takes a pointer to an int
+ *@n: pointer
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+int p, *n;
+p = 98;
+*n = &p;
+
+
+}
