3f328f820883efd818f344203086fa5b48c438a1
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-31 14:48:18 -0700

    my commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+int i;
 
-if (strcmp(s1, s2) = 0)
-return (0);
-else if (strcmp(s1, s2) < 0)
-return (-1);
-else if (strcmp(s1, s2) > 0)
-return (1);
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
 }
+i++;
+}
+return (0);
+}
+
