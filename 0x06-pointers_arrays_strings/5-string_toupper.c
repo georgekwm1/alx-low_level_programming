ab1929e2e9b2c00b442ff8d9f98912fe8ae0d73b
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-31 15:19:30 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * Description: 'the program's description'
+ * @: source pointer
+ * @: destination pointer
+ *
+ * Return: Always 0 (Success)
+ */
+char *string_toupper(char *)
+{
+char a[], t, b;
+
+t = strlwr(a);
+b = strupr(t)
+return (0);
+
+}
