a76601faa35eab781535edbafea76fa8262c89a6
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-03-31 14:27:54 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - Writes a function that compares two strings.
+ *
+ * Description: 'the program's description'
+ * @s1: 1st String
+ * @s2: 2nd String
+ *
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+strcmp(s1, s2);
+return (0);
+}
