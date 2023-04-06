56d4867ed079c099d47601cf1d87deaab06d86fb
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-04-05 20:12:05 -0700

    my commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Write a function that prints a string, then a new line.
+ *@s : Character
+ *
+ *
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+
+_putchar(s);
+
+}
