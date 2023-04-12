367ae6ff42eedf9b3c24befd7fa36a1596a3ebeb
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-04-12 12:07:08 -0700

    my commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
+ * create_array - prints buffer in hexa
+ * @c: the address of memory to print
  * @size: the size of the memory to print
  *
  * Return: Nothing.
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 char *array = malloc(size * sizeof(char));
 unsigned int i = 0;
 
-while(i < size)
+while (i < size)
 {
 array[i] = c;
 i++;
@@ -27,5 +27,5 @@ if (size == 0)
 return (NULL);
 }
 else
-return (NULL);    
+return (NULL);
 }
