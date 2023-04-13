30f0d29db1ccd77e450ad23d0f2b205342c9fe98
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-04-12 22:53:01 -0700

    my commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,71 +15,50 @@
  */
 		
 int **alloc_grid(int width, int height)
-		
+	
 {
-		
-	int **mee;
-		
-	int x, y;
-		
+	
+int **mee;
+int x, y;
+if (width <= 0 || height <= 0)
+return (NULL);
 
-		
-	if (width <= 0 || height <= 0)
-		
-		return (NULL);
-		
+mee = malloc(sizeof(int *) * height);
 
-		
-	mee = malloc(sizeof(int *) * height);
-		
+if (mee == NULL)
 
-		
-	if (mee == NULL)
-		
-		return (NULL);
-		
+return (NULL);
 
-		
-	for (x = 0; x < height; x++)
-		
-	{
-		
-		mee[x] = malloc(sizeof(int) * width);
-		
+for (x = 0; x < height; x++)
 
-		
-		if (mee[x] == NULL)
-		
-		{
-		
-			for (; x >= 0; x--)
-		
-				free(mee[x]);
-		
+{
 
-		
-			free(mee);
-		
-			return (NULL);
-		
-		}
-		
-	}
-		
+mee[x] = malloc(sizeof(int) * width);
 
-		
-	for (x = 0; x < height; x++)
-		
-	{
-		
-		for (y = 0; y < width; y++)
-		
-			mee[x][y] = 0;
-		
-	}
-		
+if (mee[x] == NULL)
 
-		
-	return (mee);
+{
+
+for (; x >= 0; x--)
+
+free(mee[x]);
+	
+free(mee);
+	
+return (NULL);
+
+}
+	
+}
+	
+for (x = 0; x < height; x++)
+	
+{
+	
+for (y = 0; y < width; y++)	
+mee[x][y] = 0;
+}
+
+return (mee);
 		
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -15,27 +15,16 @@
  * Description: frees memory of grid
 		
  * Return: nothing
-		
+
  *
-		
  */
 		
 void free_grid(int **grid, int height)
-		
 {
-		
-	int i;
-		
-
-		
-	for (i = 0; i < height; i++)
-		
-	{
-		
-		free(grid[i]);
-		
-	}
-		
-	free(grid);
-		
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
 }
