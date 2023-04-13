571275d86935df6b180dc2b75a7b29a97eb90e18
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-04-12 22:25:50 -0700

    my commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,33 +1,59 @@
-#include "main.h"
+1-strdup.c
+
+
 #include <stdio.h>
+		
 #include <stdlib.h>
-#include <string.h>
+		
+#include "main.h"
+		
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- *
- * Description: 'the program's description'
- * @str: describe the parameter
- *
- * Return: Always 0 (Success)
+		
+ * _strdup - duplicate to new memory space location
+		
+ * @str: char
+		
+ * Return: 0
+		
  */
-
+		
 char *_strdup(char *str)
+		
 {
-if (str == NULL)
-{
-return (NULL);
-}
-    
-char *dup_str = (char*) malloc(strlen(str) + 1);
-    
-if (dup_str == NULL)
-{
-return (NULL);
-}
-    
-strcpy(dup_str, str);
-    
-return (dup_str);
-free(dup_str);
-}
+		
+	char *dup_str;
+		
+	int i, r = 0;
+		
 
+		
+	if (str == NULL)
+		
+		return (NULL);
+		
+	i = 0;
+		
+	while (str[i] != '\0')
+		
+		i++;
+		
+
+		
+	aaa = malloc(sizeof(char) * (i + 1));
+		
+
+		
+	if (dup_str == NULL)
+		
+		return (NULL);
+		
+
+		
+	for (r = 0; str[r]; r++)
+		
+		dup_str[r] = str[r];
+		
+	
+return (aaa);
+		
+}
