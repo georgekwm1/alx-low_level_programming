46107f8a4fc6ec5adb589de1bdcc7fe7e5ae63e9
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-04-18 18:25:45 -0700

    my commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * _init_dog - writes a function that initializes var of type struct dog
+ *
+ * @d: structure pointer
+ * @str: describe the parameter
+ * @age: age
+ * @owner: char
+ *
+ * Return: Always 0 (Success)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog
+{
+char *name;
+float age;
+char *owner;
+} *dog;
+
+}
