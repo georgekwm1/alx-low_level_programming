ec5c1263e668f7cac15844c70d235976f52c80c7
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-04-18 19:06:09 -0700

    my commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,13 +11,12 @@
  *
  * Return: Always 0 (Success)
  */
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog
-{
-char *name;
-float age;
-char *owner;
-} *dog;
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
 
 }
