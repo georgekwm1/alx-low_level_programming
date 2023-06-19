1982708454f831bd2b4d4959450b5c4520713da4
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-06-19 16:06:02 -0700

    My Commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,2 +1,6 @@
+#ifndef 3_FUNCTION_LIKE_MACRO_H
+#define 3_FUNCTION_LIKE_MACRO_H
 #include <stdio.h>
+#include <stdlib.h>
 #define ABS(x) abs(x)
+#endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,2 +1,5 @@
+#ifndef 4_SUM_H
+#define 4_SUM_H
 #include <stdio.h>
 #define SUM(x, y) ((x) + (y))
+#endif
