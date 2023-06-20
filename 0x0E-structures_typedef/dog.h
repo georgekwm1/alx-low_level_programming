ea259aa572f275558534554f99c92a95784b42fb
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-06-19 17:10:44 -0700

    My Commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog{
+/**
+ * struct dog - frees dog
+ * @name : dog's name
+ * @age : dog's age
+ * @owner : dog's owner
+ *
+ */
+
+struct dog
+{
 char *name;
 float age;
 char *owner;
