bab59819063633988b005cc71bc48ef314382775
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2023-06-22 02:02:48 -0700

    My Commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,13 +15,14 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	int i = 0;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
 	while (i < (int)size)
 	{
 		action(array[i]);
 		i++;
-	}
-	if (array == NULL || action == NULL)
-	{
-		return;
 	}
 }
